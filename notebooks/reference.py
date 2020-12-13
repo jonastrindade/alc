f2cfa53a001af130fa4ed9040e9b394c88670c05
jonastrindade <jonastrinda@gmail.com>
2020-12-13 17:43:16 -0300

    ep1

---
+++ notebooks/reference.py
@@ -0,0 +1,6 @@
+# my reference
+# np.zeros(10, dtype=int) // create list with 10 zeros
+# np.ones(3,5) // create matrix 3x5 with ones
+# np.full((3,5), 3.14) // create matrix 3x5 with 3.14
+# np.random.randint(0,10, (3,3)) // create matrix 3x3 with int random 0 to 10
+# np.eye(3) // create identity matrix 3x3 
